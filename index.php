8cdc7bc6f6d17cfe89e41226542430ff75f63b29
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-02 19:00:38 -0500

    index estados

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    echo "hola";
+    include_once("nusoap-0.9.5/lib/nusoap.php");
+    //url del webservice
+    $wsdl="http://quickbox.com.mx/web_service_cp_municipio.php?wsdl";
+     //instanciando un nuevo objeto cliente para consumir el webservice
+    $client = new nusoap_client($wsdl,'wsdl');
+    //ELIMINIAMOS CACHE
+    ini_set("soap.wsdl_cache_enabled", "0");
+    $estados= array('Aguascalientes','BajaCalifornia','BajaCaliforniaSur','Campeche','Chiapas',
+                    'Chihuahua','Coahuila','Colima','DF','Durango','EdoMexico',
+                    'Guanajuato','Guerrero','Hidalgo','Jalisco','Michoacan',
+                    'Morelos','Nayarit','NuevoLeon','Oaxaca','Puebla',
+                    'Puebla','Queretaro','QuintanaRoo','SanLuisPotosi');
+    //GENERAMOS EL PARAMETRO DE ESTADO Y MUNICIPIO
+    $params = array(
+        "estado_cp" => $_GET['estado_cp'],
+        "municipio_nuevo" => $_GET['municipio_nuevo']
+    );
+    //llamando al método y pasándole el array con los parámetros
+    $resultado = $client->call('CONSULTA_MUNICIPIO_CP', $param);
+    print_r($resultado);
+?>
 ?>
