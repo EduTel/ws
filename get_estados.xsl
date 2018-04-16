7820675757de0b89dd9958639d5a85d3aee4a717
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-16 10:39:21 -0500

    get_xmlt

--- get_estados.xsl
+++ get_estados.xsl
@@ -1,12 +1,13 @@
 ﻿<?xml version="1.0" encoding="UTF-8"?>
     <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <?xml-stylesheet href="estados.css" title="Estilo1"?>
     <!--    
         method: define el formato de salida (xml, html o text).
         version: define la versión del formato de salida.
         enconding: juego de caracteres de salida. Por defecto UTF-8.
         indent: indenta la salida de la transformación (yes o no).
     -->
-    <xsl:output method="html" version="4.0" encoding="UTF-8" indent="yes" />
+    <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes" />
     <!-- Plantilla all -->
     <!--
     <xsl:template match="/">
@@ -20,7 +21,6 @@
             <xsl:value-of select="nombre" />
         </nombre>
     </xsl:template>
-    
     <!--
     <xsl:template match="/estados/pais/estado">
         <xsl:apply-templates />
--- server.php
+++ server.php
@@ -1,17 +1,23 @@
 <?php
     include_once("nusoap-0.9.5/lib/nusoap.php");
-    $myfile  = fopen("estados.xml", "r") or die("Unable to open file!");
-    $xmldata = "";
-    while ($line = fgets($myfile)) {
-        $xmldata.=$line;
+    function get_xmlt($name=null){
+        if($name!==null){
+            $myfile  = fopen("estados.xml", "r") or die("Unable to open file!");
+            $xmldata = "";
+            while ($line = fgets($myfile)) {
+                $xmldata.=$line;
+            }
+            fclose($myfile);
+            //echo "<pre>";
+            //echo htmlspecialchars($xmldata);
+            //echo "</pre>";
+            $xslt = new xsltProcessor;
+            $xslt->importStyleSheet(DomDocument::load($name));
+            $xslt_data = $xslt->transformToXML(DomDocument::loadXML($xmldata));
+        }
     }
-    fclose($myfile);
-    //echo "<pre>";
-    //echo htmlspecialchars($xmldata);
-    //echo "</pre>";
-    $xslt = new xsltProcessor;
-    $xslt->importStyleSheet(DomDocument::load('get_estados.xsl'));
-    $xslt_data = $xslt->transformToXML(DomDocument::loadXML($xmldata));
+    $paises  = get_xmlt('get_estados.xsl');
+    $estados = get_xmlt('get_paises.xsl');
     //echo "<pre>";
     //echo htmlspecialchars($xslt_data);
     //echo "</pre>";
@@ -23,8 +29,12 @@
     $urn = array(
         'url1'=>"mi_ws1"
     );
-    $server->configureWSDL('Web servicie de Eduardo ', 'urn:'.$urn['url1']);
+    $miURL    = 'urn : '.$urn['url1'];
+    $endpoint = 'http: //regochan.com/nusoap/soap_servidor.php';
+    $server->configureWSDL('Web servicie de Eduardo ',$miURL);
+    $server->wsdl->schemaTargetNamespace = $miURL;
     // Parametros de entrada
+    /*
     $server->wsdl->addComplexType(  'type_entrada_get_estados', 
                                     'complexType', 
                                     'struct', 
@@ -50,28 +60,26 @@
                                                             )
                                     )
                                 );
-    $server->register(  
-                    $metodos['get_estados'], // nombre del metodo o funcion
-                    array(
-                            'type_entrada_get_estados' => 'tns:type_entrada_get_estados'
-                    ), // parametros de entrada
-                    array(
-                            'return' => 'tns:type_get_estados'
-                    ), // parametros de salida
-                    'urn:'.$urn['url1'], // namespace
-                    'urn:hellowsdl2#'.$metodos['get_estados'], // soapaction debe ir asociado al nombre del metodo
-                    'rpc', // style
-                    'encoded', // use
-                    'Este método recibe el nombre del país del que quiera los estados' // documentation
-                );
-    function get_estados($pais=null) {
+    */
+     function metodo_get_estados($pais=null) {
         if($pais!==null){
             $msg = 'País: '.$pais; 
             return array(
                         'mensaje' => $msg
-                    );
+            );
         }
     }
+    $server->register(  
+                    $metodos['get_estados'], // nombre del metodo o funcion
+                    array('pais'   => 'xsd: string'), // Estructura de parámetros de entrada
+                    array('return' => 'xsd: string'), // Estructura de parámetros de salida
+                    $miURL, // namespace
+                    $miURL.'#'.$metodos['get_estados'], // soapaction debe ir asociado al nombre del metodo /*Acción soap*/
+                    'rpc', // style
+                    'encoded', // use
+                    'Este método recibe el nombre del país del que quiera los estados' // documentation
+                );
+   
     $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
     $server->service($HTTP_RAW_POST_DATA);
 ?>
