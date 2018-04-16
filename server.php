7a263f89fe3739c8396100b669938645f50d2fcd
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-16 10:57:05 -0500

    server

--- server.php
+++ server.php
@@ -108,6 +108,6 @@
                     'Este método recibe el nombre del país del que quiera los estados' // documentation
                 );
    
-    //$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-    //$server->service($HTTP_RAW_POST_DATA);
+    $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+    $server->service($HTTP_RAW_POST_DATA);
 ?>
