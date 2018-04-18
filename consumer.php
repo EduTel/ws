30f2f09e1300ede944ece31a77612b42de914c1e
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-18 10:07:33 -0500

    consumer

--- consumer.php
+++ consumer.php
@@ -1,17 +1,18 @@
 <?php
     include_once("nusoap-0.9.5/lib/nusoap.php");
     //url del webservice
-    $wsdl="http://quickbox.com.mx/web_service_cp_municipio.php?wsdl";
+    $wsdl="http://127.0.0.1/ws/server.php?wsdl";
      //instanciando un nuevo objeto cliente para consumir el webservice
     $client = new nusoap_client($wsdl,'wsdl');
     //ELIMINIAMOS CACHE
     ini_set("soap.wsdl_cache_enabled", "0");
-    //GENERAMOS EL PARAMETRO DE ESTADO Y MUNICIPIO
-    $params = array(
-        "estado_cp" => $_GET['estado_cp'],
-        "municipio_nuevo" => $_GET['municipio_nuevo']
-    );
     //llamando al método y pasándole el array con los parámetros
-    $resultado = $client->call('CONSULTA_MUNICIPIO_CP', $param);
+    $resultado = $client->call('metodo_get_paises');
     print_r($resultado);
+    //GENERAMOS EL PARAMETRO DE ESTADO Y MUNICIPIO
+    $params = array('mexico');
+    $resultado = $client->call('metodo_get_estados', $params);
+    echo "<pre>";
+    echo htmlspecialchars($resultado);
+    echo "</pre>";
 ?>
